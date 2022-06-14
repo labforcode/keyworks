a19d7972a9ca5b6470e52956da52c3b2c1ee3194
labforcode <labforcode@gmail.com>
2022-06-14 00:15:09 -0300

    Criação entidade Cliente
    Entidade Cliente criada parcialmente.

---
+++ src/TesteKeyworks.Cadastros.Dominios/Entidades/Clientes/Cliente.cs
@@ -0,0 +1,35 @@
+﻿namespace TesteKeyworks.Cadastros.Dominios.Entidades.Clientes
+{
+    public class Cliente
+    {
+        public Cliente(int codigo,
+                       string cnpj,
+                       string razaoSocial,
+                       bool ativo,
+                       int plantaId,
+                       int tipoCliente)
+        {
+            Codigo = codigo;
+            Cnpj = cnpj;
+            RazaoSocial = razaoSocial;
+            Ativo = ativo;
+            PlantaId = plantaId;
+            TipoCliente = tipoCliente;
+        }
+
+        protected Cliente() { }
+
+        public int Codigo { get; private set; }
+
+        public string Cnpj { get; private set; }
+
+        public string RazaoSocial { get; private set; }
+
+        public bool Ativo { get; private set; }
+
+        public int PlantaId { get; private set; }
+
+        public int TipoCliente { get; private set; }
+
+    }
+}
