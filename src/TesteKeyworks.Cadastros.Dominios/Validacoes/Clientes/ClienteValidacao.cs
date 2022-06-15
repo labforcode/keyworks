8f5b1ad3ee0e6d6056c0fc869f9034022fc9e28f
labforcode <labforcode@gmail.com>
2022-06-14 22:07:05 -0300

    Criação de Validações
    Criação de validações parcial.

---
+++ src/TesteKeyworks.Cadastros.Dominios/Validacoes/Clientes/ClienteValidacao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteKeyworks.Cadastros.Dominios.Validacoes.Clientes
+{
+    public class ClienteValidacao
+    {
+    }
+}
