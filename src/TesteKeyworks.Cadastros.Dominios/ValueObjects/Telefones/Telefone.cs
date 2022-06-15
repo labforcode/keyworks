fd818830da5aca37a604f9ff1790ebb1d74071fa
labforcode <labforcode@gmail.com>
2022-06-14 22:07:57 -0300

    Criação de value objetcs
    Criado os objetos de valor que fazem parte da entidade pessoa.

---
+++ src/TesteKeyworks.Cadastros.Dominios/ValueObjects/Telefones/Telefone.cs
@@ -0,0 +1,20 @@
+﻿namespace TesteKeyworks.Cadastros.Dominios.ValueObjects.Telefones
+{
+    public class Telefone
+    {
+        public Telefone(string ddi,
+                        string ddd,
+                        string numero)
+        {
+            DDI = ddi;
+            DDD = ddd;
+            Numero = numero;
+        }
+
+        public string DDI { get; private set; }
+
+        public string DDD { get; private set; }
+
+        public string Numero { get; private set; }
+    }
+}
