3efa6ac0d0f532c9b99171afcbd42be592e3398e
labforcode <labforcode@gmail.com>
2022-06-16 13:29:29 -0300

    Refatoração de domínio
    Refatorado para notificar o front em caso de inconsistencia no domínio.

---
+++ src/TesteKeyworks.Cadastros.Dominios.Core/Notificacoes/Notificacao.cs
@@ -0,0 +1,22 @@
+﻿namespace TesteKeyworks.Cadastros.Dominios.Core.Notificacoes
+{
+    public class Notificacao
+    {
+        private static List<string> _notificacoes { get; set; }
+
+        public static void RegistrarNotificacao(string mensagem)
+        {
+            _notificacoes = new List<string> { mensagem };
+        }
+
+        public static string ObterNotificacao()
+        {
+            return _notificacoes.FirstOrDefault();
+        }
+
+        public static bool VerificarSeHaNotificacao()
+        {
+            return _notificacoes.Any();
+        }
+    }
+}
