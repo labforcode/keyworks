59f2f4a1779da011ea8118f929e8f75f4ecf96dd
labforcode <labforcode@gmail.com>
2022-06-18 13:58:54 -0300

    Refatoração visual.

--- src/TesteKeyworks.WebApp.MVC/Controllers/ClienteController.cs
+++ src/TesteKeyworks.WebApp.MVC/Controllers/ClienteController.cs
@@ -13,71 +13,73 @@ namespace TesteKeyworks.WebApp.MVC.Controllers
             _clienteServico = clienteServico;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
-            //cadastro
-            var cliente = new ClienteDto
-            {
-                Cnpj = "12345678901236",
-                RazaoSocial = "Teste 3",
-                PlantaId = 1,
-                TipoCliente = 1,
-                CodigoMaterial = 1,
-                Responsavel = new ResponsavelDto { Email = "teste3@teste.com.br", Nome = "Cadastro Teste" },
-                Telefone = new TelefoneDto { DDI = "55", DDD = "43", Numero = "988106324" },
-            };
+   
 
-            _clienteServico.Adicionar(cliente);
+            return View();
+        }
 
-
-            //var aco = new MaterialDto 
-            //{
-            //    Descricao = "aço"
-            //};
-
-            //_materialServico.Adicionar(aco);
-
-            //var aluminio = new MaterialDto
-            //{
-            //    Descricao = "alumínio"
-            //};
-
-            //_materialServico.Adicionar(aluminio);
-
-            //var ferro = new MaterialDto
-            //{
-            //    Descricao = "ferro"
-            //};
-
-            //_materialServico.Adicionar(ferro);
-
-            //var zinco = new MaterialDto
-            //{
-            //    Descricao = "zinco"
-            //};
-
-            //_materialServico.Adicionar(zinco);
-
-            //var magnesio = new MaterialDto
-            //{
-            //    Descricao = "magnésio"
-            //};
-
-            //_materialServico.Adicionar(magnesio);
-
-            //var cobre = new MaterialDto
-            //{
-            //    Descricao = "cobre"
-            //};
-
-            //_materialServico.Adicionar(cobre);
-
-
-
-
+        [HttpGet]
+        public IActionResult PesquisarCliente(int codigo)
+        {
 
 
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult CadastrarCliente()
+        {
+
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult AtualizarCliente()
+        {
+
+
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public IActionResult CadastrarCliente([FromBody] ClienteDto cliente)
+        {
+            ////cadastro
+            //var cliente = new ClienteDto
+            //{
+            //    Cnpj = "12345678901236",
+            //    RazaoSocial = "Teste 3",
+            //    PlantaId = 1,
+            //    TipoCliente = 1,
+            //    CodigoMaterial = 1,
+            //    Responsavel = new ResponsavelDto { Email = "teste3@teste.com.br", Nome = "Cadastro Teste" },
+            //    Telefone = new TelefoneDto { DDI = "55", DDD = "43", Numero = "988106324" },
+            //};
+
+            //_clienteServico.Adicionar(cliente);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AtualizarCliente([FromBody] ClienteDto cliente)
+        {
+
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult ExcluirCliente(int codigo)
+        {
+
+
+            return Ok();
         }
     }
 }
